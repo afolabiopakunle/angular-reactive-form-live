3c9617e145acb03633463de403173730b36fae42
afolabiopakunle <opakunleafolabi@gmail.com>
2021-09-16 21:05:59 +0100

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectTitle: new FormControl(null, [Validators.required]),
+      email: new FormControl(),
+      projectStatus: new FormControl("1")
+    })
+  }
+
+  saveForm() {
+    console.log(this.projectForm.value);
+  }
 }
